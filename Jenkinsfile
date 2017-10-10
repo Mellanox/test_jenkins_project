add3a5b30fa6f622bd5918eea28a93dd667297a4
Michael Braverman <michaelbr@mellanox.com>
2017-10-10 10:58:31 +0300

    t branch -D master  # Deletes the master branch git branch -m master  # Rename the current branch to master git push -f origin master  # Force push master branch to github git gc --aggressive --prune=all     # remove the old files

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+def nodes = ['hpc-test-node', 'hpc-test-node2'] 
+git url: 'https://github.com/Mellanox/test_jenkins_projec'
+def projects = sh test_pipeline.sh get_job_list
+
+def builders = [:]
+
+for ( x in projects ) {
+  def proj = x
+  builders[proj] = {
+    node(hpc-test-node) {
+       sh test_pipeline.sh do_job ${proj}
+    }
+  }
+  
+}
---
+++ test_pipeline.sh
@@ -0,0 +1,21 @@
+action=$1
+job=$2
+
+function get_job_list () {
+  echo "a b c d e"
+}
+
+function do_job () {
+  local job_name=$1
+  local sleep_time
+  
+  sleep_time=$(( ( RANDOM % 20 )  + 1 ))
+  echo "Executing job \"${job_name}\", sleeping ${sleep_time}"
+  sleep ${sleep_time}
+}
+
+if [ "${action}" == "get_job_list" ] ; then
+  get_job_list
+elif [ "${action}" == "do_job" ] ; then
+  do_job ${job}
+fi
